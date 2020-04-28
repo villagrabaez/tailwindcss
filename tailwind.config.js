ccd2b94c71eeef9a3c7ba79b05ca55b6287270c5
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-04-27 20:58:37 -0400

    Proyecto con tailwildcss finalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary' : '#353535',
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem',
+      }
+    },
   },
   variants: {},
   plugins: [],
